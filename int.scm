edb9563444cae632f140d1568c181058da13d35e
Michael DeWitt <md@hackfoolery.com>
2013-02-02 03:40:00 -0500

    FUCK MICHAEL

---
+++ int.scm
@@ -0,0 +1,43 @@
+(load "helpers.scm")
+(load "pmatch.scm")
+
+(define env
+  (make-parameter '()))
+
+(define extend-env
+  (lambda (var val)
+    (env (cons (cons var val) (env)))))
+
+(define change-env
+  (lambda (var val)
+    (set-box! (apply-env var) val)))
+
+(define apply-env
+  (lambda (x)
+    (cdr (assq x (env)))))
+
+(define parser
+  (lambda (wds)
+    (map (lambda (x) 
+	   (if (string->number x) (string->number x) (string->symbol x)))
+	 wds)))
+
+(define int
+  (lambda (e)
+    (pmatch 
+     (parser (words e))
+     [(,var R ,val) (change-env var val)]
+     [(,n1 UP ,n2) (+ n1 n2)]
+     [(,n1 NERF ,n2) (- n1 n2)]
+     [(,n1 TIEMZ ,n2) (* n1 n2)]
+     [(,n1 OVAR ,n2) (/ n1 n2)]
+     [(,FUCK ,MICHAEL) (guard (equal? FUCK 'FUCK) (equal? MICHAEL 'MICHAEL))
+      (begin (printf "LOL. RAGEQUIT. \n")
+	     (int "FUCK MICHAEL"))] 
+     [(,n) (guard (number? n)) n]
+     [(,x) (guard (symbol? x)) (unbox (apply-env x))]
+     [(I HAZ A ,var ITS ,val) (guard (symbol? var))
+      (extend-env var (box val))]
+     [(I HAZ A ,var) (guard (symbol? var))
+      (extend-env var (box 'null))])))
+	    
