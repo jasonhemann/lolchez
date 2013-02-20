5673829ffb3ee829081731f7dab6444b10c704d1
Michael DeWitt <md@hackfoolery.com>
2013-02-20 13:42:55 -0500

    NOW HAZ REPL

--- repl.scm
+++ repl.scm
@@ -7,14 +7,24 @@
   (lambda ()
     (let readc ([s ""]
                 [c (read-char)])
-      (if (or (equal? c #\newline)
-              (eof-object? c))
-        s
-        (readc (string-append s (string c))
-               (read-char))))))
+      (cond
+        [(eof-object? c) c]
+        [(equal? c #\newline) s]
+        [(readc (string-append s (string c))
+                (read-char))]))))
 
 
 ;; REPL should track the last value of IT and take care of building complete 
 ;; expressions before handing it over to the interpreter. Parser will signal if 
 ;; it has parsed a complete expression or if it is waiting to find the end of 
 ;; a block.
+
+(define lol-repl
+  (lambda ()
+    (let repl ([input (read-line)])
+      (cond
+        [(eof-object? input) (printf "BAI!~n")]
+        [(= 0 (string-length input)) (repl (read-line))]
+        [else (begin (write (int (parse input)))
+                     (newline)
+                     (repl (read-line)))]))))
