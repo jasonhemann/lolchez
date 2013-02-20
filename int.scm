4218426c72af40b68831e9eebf62b1dcad63fbc5
Michael DeWitt <md@hackfoolery.com>
2013-02-19 23:53:21 -0500

    gg=G
    Because Josh and his emacs screwed up all the indentation.

--- int.scm
+++ int.scm
@@ -25,20 +25,19 @@
 
 (define int
   (lambda (e)
-    (pmatch 
-     (parser (words e))
-     [(,var R ,val) (change-env var val)]
-     [(,n1 UP ,n2) (+ n1 n2)]
-     [(,n1 NERF ,n2) (- n1 n2)]
-     [(,n1 TIEMZ ,n2) (* n1 n2)]
-     [(,n1 OVAR ,n2) (/ n1 n2)]
-     [(,FUCK ,MICHAEL) (guard (equal? FUCK 'FUCK) (equal? MICHAEL 'MICHAEL))
-      (begin (printf "LOL. RAGEQUIT. \n")
-	     (int "FUCK MICHAEL"))] 
-     [(,n) (guard (number? n)) n]
-     [(,x) (guard (symbol? x)) (unbox (apply-env x))]
-     [(I HAZ A ,var ITS ,val) (guard (symbol? var))
-      (extend-env var (box val))]
-     [(I HAZ A ,var) (guard (symbol? var))
-      (extend-env var (box 'null))])))
-	    
+    (pmatch (parser (words e))
+      [(,var R ,val) (change-env var val)]
+      [(,n1 UP ,n2) (+ n1 n2)]
+      [(,n1 NERF ,n2) (- n1 n2)]
+      [(,n1 TIEMZ ,n2) (* n1 n2)]
+      [(,n1 OVAR ,n2) (/ n1 n2)]
+      [(,FUCK ,MICHAEL) (guard (equal? FUCK 'FUCK) (equal? MICHAEL 'MICHAEL))
+                        (begin (printf "LOL. RAGEQUIT. \n")
+                               (int "FUCK MICHAEL"))] 
+      [(,n) (guard (number? n)) n]
+      [(,x) (guard (symbol? x)) (unbox (apply-env x))]
+      [(I HAZ A ,var ITS ,val) (guard (symbol? var))
+                               (extend-env var (box val))]
+      [(I HAZ A ,var) (guard (symbol? var))
+                      (extend-env var (box 'null))])))
+
